062f6b86f77852371b1a32de6cf9b6bd828cc0c7
alokshr <alokshrest@gmail.com>
2024-11-02 15:56:13 -0500

    Fixed search filter being case-sensitive

--- src/Browse.js
+++ src/Browse.js
@@ -57,7 +57,7 @@ function Browse({ cart, setCart }) {
 
     // Filter catalog
     const updatedCatalog = catalog.filter((item) => {
-      if (item.title.toLowerCase().includes(search)) {
+      if (item.title.toLowerCase().includes(search.toLowerCase())) {
         return true;
       }
 
