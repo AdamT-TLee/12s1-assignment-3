862c17deb08a04421f89a05f369b40ec88daa969
Alok Shrestha <aloks@iastate.edu>
2024-11-03 14:53:53 -0600

    Fixed browse page cards

--- src/Browse.js
+++ src/Browse.js
@@ -43,7 +43,7 @@ function Browse({ cart, setCart, catalog }) {
     const updatedCatalog = catalog.filter((item) => {
       for (let term of searchTerms) {
         if (
-          !item.title.toLowerCase().trim().includes(term.toLowerCase().trim())
+          !item.name.toLowerCase().trim().includes(term.toLowerCase().trim())
         ) {
           return false;
         }
@@ -60,21 +60,25 @@ function Browse({ cart, setCart, catalog }) {
   const shoppingItems = viewCatalog.map((item) => (
     <div className="col justify-content-center" key={item.id}>
       <div className="card w-100 h-100">
-        <div className="card-header bg-white">
+        <div className="card-header p-0 bg-white align-content-center">
           <img
             src={item.image.url}
-            className="card-img-top img-fluid p-4 object-fit-contain"
-            style={{ maxHeight: "27rem", minHeight: "27rem" }}
-            alt="..."
+            className="card-img-top img-fluid object-fit-contain p-2"
+            style={{ maxHeight: "24rem"}}
+            alt={`${item.image.author}`}
           />
         </div>
 
         <div className="card-body">
           <h5 className="card-title">
-            <span className="px-2 bg-success rounded-4 text-light">
-              ${item.price}
-            </span>{" "}
-            {item.title}
+            <div className="row">
+              <div className="col">
+                <span className="px-2 bg-success rounded-4 text-light">
+                  ${item.price}
+                </span>
+                {` ${item.name}`}
+              </div>
+            </div>
           </h5>
           <hr />
           <div className="my-3 card-text">{item.description}</div>
